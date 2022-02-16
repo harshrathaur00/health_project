19f5fd60b62b7481deaf73c66cbb6ecfe5e2351e
harshrathaur <55021326+harshrathaur@users.noreply.github.com>
2022-02-16 10:12:33 +0530

    created

---
+++ src/main/java/com/example/health/HealthcareProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareProjectApplication.class, args);
+	}
+
+}
